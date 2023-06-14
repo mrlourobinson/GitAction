a387536de2d699020517cc65d348f6aecf396ee1
Lou Robinson <hello@lourobinson.co.uk>
2023-06-14 12:15:06 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,17 +1,76 @@
-import datetime
-from datetime import date
+import xarray as xr
 import pandas as pd
-import numpy as np
+import regionmask
+from zipfile import ZipFile
+import cdsapi
 
-datetime = datetime.datetime.now()
-datetime = datetime.strftime("%Y-%m-%d_%H-%M-%S")
-print("Today's date: " + str(datetime))
+now = datetime.now() - timedelta(days=5)
 
-rand = np.random.randint(0,1000)
+year = str(now.strftime("%Y"))
+month = str(now.strftime("%m"))
+day = str(now.strftime("%d"))
 
-row = [{'date':datetime, 
-       'rand':rand}]
+uid = '199339'
+apikey = '15aa7d28-4258-4669-bd30-2cb1128d0a57'
 
-df = pd.DataFrame(row)
+c = cdsapi.Client(key=f"{uid}:{apikey}", url="https://cds.climate.copernicus.eu/api/v2")
 
-df.to_csv('data/'+str(datetime)+'.csv')
+
+c.retrieve(
+    'derived-utci-historical',
+    {
+        'variable': 'universal_thermal_climate_index',
+        'version': '1_1',
+        'product_type': 'intermediate_dataset',
+        'year': year,
+        'month': [
+            month,
+        ],
+        'day': [
+            day,
+        ],
+        'format': 'zip',
+    },
+    'download.zip')
+
+
+with ZipFile('/work/download.zip', 'r') as f:
+
+    #extract in current directory
+    f.extractall()
+
+year = str(now.strftime("%Y%m%d"))
+filename = 'ECMWF_utci_'+year+'_v1.1_int.nc'
+
+# Merge all rasters into one file
+clim_max = xr.open_dataset('/work/'+filename)
+
+# get the max of each cell
+clim_max = clim_max.max('time')
+
+# Write to new netCDF4 file
+clim_max.to_netcdf("clim_max.nc")
+
+
+collated = []
+
+clim = clim_max
+
+utci_max = clim.utci.to_dataframe().reset_index()['utci'].max() - 273.15
+utci_min = clim.utci.to_dataframe().reset_index()['utci'].min() - 273.15
+utci_mean = clim.utci.to_dataframe().reset_index()['utci'].mean() - 273.15
+
+temp = pd.DataFrame({
+    'datetime': pd.to_datetime([now], format='%d%b%Y:%H:%M:%S.%f'),
+    'max-utci': [utci_max],
+    'min-utci': [utci_min],
+    'mean-utci':[utci_mean],
+    })
+
+collated.append(temp)
+
+
+collated = pd.concat(collated)
+print(collated)
+
+collated.to_csv('data/'+str(now)+'.csv')
